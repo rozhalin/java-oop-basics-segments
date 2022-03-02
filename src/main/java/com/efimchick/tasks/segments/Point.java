e9987294a564be4a366f6aeee4ad6f5d18e96f50
Roman Rozhalin <rozromario@gmail.com>
2022-03-02 17:56:02 +0300

    -all tests are passed

--- src/main/java/com/efimchick/tasks/segments/Point.java
+++ src/main/java/com/efimchick/tasks/segments/Point.java
@@ -1,5 +1,7 @@
 package com.efimchick.tasks.segments;
 
+import static com.efimchick.tasks.segments.MathConstant.EPSILON;
+
 class Point {
     private double x;
     private double y;
@@ -15,5 +17,15 @@ class Point {
 
     public double getY() {
         return y;
+    }
+
+    public int compareTo(Point p) {
+        if (Math.abs(this.x - p.getX()) < EPSILON && Math.abs(this.y - p.getY()) < EPSILON) {
+            return 0;
+        } else if (this.x > p.getX()) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
